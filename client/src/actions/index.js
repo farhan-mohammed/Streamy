492ab5054da231efe87be14965a0304cdd0bfd0d
Farhan-on-Desktop <farhanhm12@gmail.com>
2019-12-19 06:12:35 -0500

    added json server

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,13 +1,22 @@
-import {SIGN_IN,SIGN_OUT} from'./types'
-export const signIn =(userId)=>{
-    return {
-        type:SIGN_IN,
-        payload:userId
-    }
-}
+import streams from '../apis/streams';
+import { SIGN_IN, SIGN_OUT } from './types';
+export const signIn = (userId) => {
+	return {
+		type: SIGN_IN,
+		payload: userId
+	};
+};
 
-export const signOut =()=>{
-    return {
-        type:SIGN_OUT
-    }
-}
+export const signOut = () => {
+	return {
+		type: SIGN_OUT
+	};
+};
+// export const createStream =(formvalues)=>{
+//     return async (dispatchEvent)=>{
+
+//     }
+// }
+export const createStream = (formvalues) => async (dispatchEvent) => {
+	streams.post('/streams', formvalues);
+};
